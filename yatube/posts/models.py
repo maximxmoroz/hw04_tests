fe8523de6f59b11c202f74c6d161a685a097c4ea
Максим Морозов <maximxmoroz@MacBook-Air-Maksim.local>
2022-02-27 13:06:49 +0300

    4

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,51 +5,38 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Название группы')
-    slug = models.SlugField(
-        unique=True,
-        max_length=200, verbose_name='Строка для уникального URL-адреса'
-    )
-    description = models.TextField(verbose_name='Описание')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        verbose_name = 'Группа'
-        verbose_name_plural = 'Группы'
-
 
 class Post(models.Model):
-    text = models.TextField(
-        null=True,
-        max_length=400,
-        verbose_name='Текст поста',
-        help_text='Текст нового поста',
-    )
-    pub_date = models.DateTimeField(
-        uto_now_add=True, verbose_name='Дата публикации'
-    )
+    text = models.TextField(verbose_name='Текст поста',
+                            help_text='Введите текст поста',)
+    pub_date = models.DateTimeField(verbose_name='Дата публикации',
+                                    auto_now_add=True,)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts',
-        verbose_name='Автор'
-    )
-    group = models.ForeignKey(
-        Group,
-        on_delete=models.SET_NULL,
-        related_name='posts',
-        blank=True,
-        null=True,
-        verbose_name='Группа',
-        help_text='Группа поста'
+        verbose_name='Автор',
     )
 
     def __str__(self):
-        return self.text[:20]
+        return self.text[:15]
+
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='posts',
+        verbose_name='Группа',
+        help_text='Выберите группу',
+    )
 
     class Meta:
-        verbose_name_plural = 'Посты'
-        verbose_name = 'Пост'
         ordering = ('-pub_date',)
